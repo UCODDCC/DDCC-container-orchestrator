2175b57d90cd55c59be57def220e796c38d958be
diego@alpha <diegorodriguezriera@gmail.com>
2020-03-28 13:29:10 +0100

    fixed #1 basic server

--- container_orchestrator/main.py
+++ container_orchestrator/main.py
@@ -1,5 +1,15 @@
+from container_orchestrator.tcp.TcpServer import TcpServer
+
+def setup():
+    global server
+    print("init!")
+    server = TcpServer(7655)
+
+
 def loop():
-    print("hello, world")
+    global server
+    print("loop!")
+    server.handle_next_connection()
 
 
 
